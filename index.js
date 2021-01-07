4681bc00415ef739ab3968cc58d498eeb8de3039
bestPlayerXu <73661874+bestPlayerXu@users.noreply.github.com>
2021-01-07 08:01:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const port = 3000;
 
 
 app.listen(port, () => console.log(`Example app listening at port ${port}`));
-app.get('/', (req, res) => res.sendStatus(200));
+app.get('/', (req, res) => res.sendStatus(200));*/
 
 const Discord = require('discord.js');
-const client = new Discord.Client();*/
+const client = new Discord.Client();
 const fs = require('fs');
 /*const DBL_API = require('dblapi.js');
 const dbl = new DBL_API(process.env.DBL_TOKEN, client);
