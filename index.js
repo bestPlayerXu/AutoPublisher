248803366329beedaea2d9b63b2d164d5f74a6af
bestPlayerXu <73661874+bestPlayerXu@users.noreply.github.com>
2021-01-07 09:05:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 3000;
 app.listen(port, () => console.log(`Example app listening at port ${port}`));
 app.get('/', (req, res) => res.sendStatus(200));*/
 
+process.title = 'AutoPublisher';
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
